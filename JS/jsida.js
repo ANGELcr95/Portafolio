b8f8147b5c1d474e029307efbdd882227a314b62
Miguel Angel <miguel-angelcr@hotmail.com>
2021-06-29 23:29:04 -0500

    up animation

---
+++ JS/jsida.js
@@ -0,0 +1,56 @@
+        // Declariacion de variables globales 
+let up = document.querySelectorAll('.works a')
+let colour = document.getElementById('primeroida')
+        
+        // Animacion de ida
+let transition = (color) => {
+    if(window.scrollY == 0 && color == 1){
+        document.getElementById('primeroida').classList.add('unoida');
+        document.getElementById('segundoida').classList.add('dosida');
+        document.getElementById('spinprincipalida').classList.add('spinida');
+        document.getElementById('spinunoida').classList.add('spin1ida');
+        document.getElementById('spindosida').classList.add('spin2ida');
+        document.getElementById('spintresida').classList.add('spin3ida');
+        setTimeout( () => {window.location="https://trivia2-mu.vercel.app/";},2250)
+    }
+    if(window.scrollY == 0 && color == 2){
+        document.getElementById('primeroida').classList.add('unoida');
+        document.getElementById('segundoida').classList.add('dosida');
+        document.getElementById('spinprincipalida').classList.add('spinida');
+        document.getElementById('spinunoida').classList.add('spin1ida');
+        document.getElementById('spindosida').classList.add('spin2ida');
+        document.getElementById('spintresida').classList.add('spin3ida');
+        setTimeout( () => {window.location="https://quotereact-kappa.vercel.app/";},2250)
+    }
+    if(window.scrollY == 0 && color == 3){
+        document.getElementById('primeroida').classList.add('unoida');
+        document.getElementById('segundoida').classList.add('dosida');
+        document.getElementById('spinprincipalida').classList.add('spinida');
+        document.getElementById('spinunoida').classList.add('spin1ida');
+        document.getElementById('spindosida').classList.add('spin2ida');
+        document.getElementById('spintresida').classList.add('spin3ida');
+        setTimeout( () => {window.location="https://automatica-sas.vercel.app/";},2250)
+    }  
+}
+
+document.addEventListener('scroll', transition)
+
+        // Recorre los a de work en el assets y a esos a agregarles un evento para que ejecute una funcion anonima tipo flecha
+
+up.forEach((a) => {
+    a.addEventListener('click', (e) => {
+        setTimeout( () => {transition(e.target.name)},1000)
+    })
+})
+
+function generarNuevoColor(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+	}
+	colour.style.background = color;
+}
+
+generarNuevoColor()
